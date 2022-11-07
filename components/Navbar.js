83fe69875d816313282de69506aec91968dc3fa7
donblizy <nbliziotis@icloud.com>
2022-11-07 12:35:07 +0000

    initial setup - work on nav

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="fixed h-14 w-full flex flex-nowrap items-center p-4 bg-[#171718] mb-[2px] z-10">
+      <div className="flex grow justify-start items-center"></div>
+      {/* Middle */}
+      {/* Right  */}
+    </div>
+  );
+};
+
+export default Navbar;
